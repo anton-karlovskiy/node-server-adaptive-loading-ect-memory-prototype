6a6d5402a3eefd5a5d42091366815faff2d57109
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-09-12 18:31:04 +0300

    updated firebase functions

--- functions/index.js
+++ functions/index.js
@@ -11,7 +11,6 @@ const express = require('express');
 // const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
-const request = require('request');
 
 const app = express();
 app.disable('x-powered-by');
@@ -25,19 +24,14 @@ app.get('/ping', (req, res) => {
   res.send('pong');
 });
 
-app.get('/memory-considerate-image', (req, res) => {
+app.get('/network-memory-considerate-model', (req, res) => {
+  const ect = req.headers.ect;
   const deviceMemory = req.headers['device-memory'];
-  console.log('[server memory-considerate-image request] Device Memory => ', deviceMemory);
-  const url = deviceMemory < 1 ?
-    'https://cdn.glitch.com/8d7fb7f0-a9be-4a8c-96c7-8af286af487e%2Fmin-res.jpg?v=1562842586912' :
-    'https://cdn.glitch.com/8d7fb7f0-a9be-4a8c-96c7-8af286af487e%2Fmax-res.jpg?v=1562842587982';
-
-  try {
-    request.get(url).pipe(res);
-  } catch (error) {
-    console.log('[server memory-considerate-image request proxy] error => ', error);
-    res.json({error});
-  }
+  console.log('[server network-memory-considerate-model request] ECT, Device Memory => ', ect, deviceMemory);
+  
+  // TODO: As this is a demo, I think it should be easy enough to change these numbers as needed in the future. -> dotenv
+  const experienceType = ect === '4g' && deviceMemory > 4 ? 'heavy' : 'light';
+  res.json({experienceType});
 });
 
 app.use(express.static(path.join(__dirname, 'build')));
